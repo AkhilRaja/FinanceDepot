0b10dd085c984b048eaab5c73f220bfb83028982
Akhil Raja <p.akhilraja@gmail.com>
2020-06-02 16:30:38 +0530

    Base project

---
+++ app/src/main/java/com/mismo/news/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mismo.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mismo.news.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
